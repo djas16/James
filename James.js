d8e120943adcda359f2e604ca44b0e0c6c3ffed6
djas16 <45173917+djas16@users.noreply.github.com>
2019-09-24 14:37:56 +0100

    Add files via upload

---
+++ James.js
@@ -0,0 +1,10 @@
+public class James {
+  public James() {
+    System.out.println("I have created a New James");
+  }
+
+
+  public void abu() {
+    System.out.println("I have created a new Abubakr");
+  }
+}
---
+++ main.js
@@ -0,0 +1,19 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+    goodbye(7);
+    dylan(7);
+    James newvar = new James();
+    newvar.abu();
+  }
+
+  public static String goodbye(int age){
+    System.out.println("Goodbye"); 
+    return "I said Goodbye";
+  }
+  
+  public static String dylan(int age){
+    System.out.println("Dylan is Gay"); 
+    return "I called Dylan,Gay";
+  }
+}
